99f48063664fab84ba10cbace8c90425e47fa9e5
juan salim <juandsalim@gmail.com>
2023-06-13 22:43:14 -0300

    .

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse("This is my first url")
+
+
+def specific(request):
+    return HttpResponse("this is the specific url")
+
+def article(request,article_id):
+    return HttpResponse(article_id)
+    
